d3430912c21a7386f953071452218d9e8d426642
LYChubby <f4uzi345@gmail.com>
2025-05-08 20:16:09 +0700

    Membuat Fungsi Jika Complete = Green, Not Completed = Red

--- lib/presentation/todo_page.dart
+++ lib/presentation/todo_page.dart
@@ -123,6 +123,8 @@ class TodoPage extends StatelessWidget {
                                 Text(todo.title, style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold)),
                                 SizedBox(height: 4.0),
                                 Text('${todo.date.day}/${todo.date.month}/${todo.date.year}', style: TextStyle(color: Colors.grey)),
+                                SizedBox(height: 4.0),
+                                Text(todo.isCompleted ? 'Completed' : 'Not Completed', style: TextStyle(color: todo.isCompleted ? Colors.green : Colors.red)),
                               ],
                             )
                           ],),
